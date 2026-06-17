7d45e460d0c59e137270d6bbd32671534d67e073
h4llowOs <comicalhallow@gmail.com>
2026-06-16 19:01:55 -0700

    Remove website layout attribution from List.js
    Removed the website layout attribution paragraph.

--- js/pages/List.js
+++ js/pages/List.js
@@ -123,7 +123,6 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
